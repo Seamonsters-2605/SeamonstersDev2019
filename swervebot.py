0d871add310c3c129adb9841289a2982af65f26d
Jacob van't Hoog <vantjac@gmail.com>
2018-08-28 12:49:15 -0700

    Update swervebot for new swerve drive bot

--- swervebot.py
+++ swervebot.py
@@ -10,52 +10,44 @@ class SwerveBot(sea.GeneratorBot):
     def robotInit(self):
         self.joystick = wpilib.Joystick(0)
 
-        wheelADriveTalon = ctre.WPI_TalonSRX(11)
-        wheelARotateTalon = ctre.WPI_TalonSRX(12)
+        wheelADriveTalon = ctre.WPI_TalonSRX(1)
+        wheelARotateTalon = ctre.WPI_TalonSRX(0)
 
-        wheelBDriveTalon = ctre.WPI_TalonSRX(13)
-        wheelBRotateTalon = ctre.WPI_TalonSRX(14)
-
-        wheelCDriveTalon = ctre.WPI_TalonSRX(15)
-        wheelCRotateTalon = ctre.WPI_TalonSRX(16)
+        wheelBDriveTalon = ctre.WPI_TalonSRX(3)
+        wheelBRotateTalon = ctre.WPI_TalonSRX(2)
 
         for talon in [wheelADriveTalon, wheelARotateTalon,
-                      wheelBDriveTalon,wheelBRotateTalon,
-                      wheelCDriveTalon,wheelCRotateTalon]:
+                      wheelBDriveTalon,wheelBRotateTalon]:
             talon.configSelectedFeedbackSensor(
                 ctre.FeedbackDevice.QuadEncoder, 0, 0)
 
-        wheelADrive = sea.AngledWheel(wheelADriveTalon,0, 1, 0,
-                                      encoderCountsPerFoot=6000,
+        wheelADrive = sea.AngledWheel(wheelADriveTalon,.75, .75, 0,
+                                      encoderCountsPerFoot=31291,
                                       maxVoltageVelocity=12)
-        wheelBDrive = sea.AngledWheel(wheelBDriveTalon,1,0,0,
-                                      encoderCountsPerFoot=6000,
-                                      maxVoltageVelocity=12)
-        wheelCDrive = sea.AngledWheel(wheelCDriveTalon,-1,0,0,
-                                      encoderCountsPerFoot=6000,
+        wheelBDrive = sea.AngledWheel(wheelBDriveTalon,-.75,.75,0,
+                                      encoderCountsPerFoot=31291,
                                       maxVoltageVelocity=12)
 
         wheelARotate = sea.SwerveWheel(wheelADrive, wheelARotateTalon,
-                                       3138, True)
+                                       1613, True)
         wheelBRotate = sea.SwerveWheel(wheelBDrive,wheelBRotateTalon,
-                                       3138, True)
-        wheelCRotate = sea.SwerveWheel(wheelCDrive,wheelCRotateTalon,
-                                       3138, True)
+                                       1613, True)
+
         self.superDrive = sea.SuperHolonomicDrive()
 
-        for wheelrotate in [wheelARotate,wheelBRotate,wheelCRotate]:
+        for wheelrotate in [wheelARotate,wheelBRotate]:
             self.superDrive.addWheel(wheelrotate)
 
         for wheel in self.superDrive.wheels:
-            wheel.driveMode = ctre.ControlMode.Velocity
+            wheel.driveMode = ctre.ControlMode.PercentOutput
 
     def teleop(self):
         while True:
             mag = sea.deadZone(self.joystick.getMagnitude())
-            mag *= 5 # maximum feet per second
+            mag *= 4 # maximum feet per second
             direction = -self.joystick.getDirectionRadians() - math.pi/2
             turn = sea.deadZone(self.joystick.getTwist())
-            turn *= math.radians(90) # maximum radians per second
+            turn *= math.radians(120) # maximum radians per second
 
             self.superDrive.drive(mag, direction, turn)
 
