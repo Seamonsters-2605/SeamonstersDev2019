ddd878d1c589052692d38d8d42ee467963b808b5
LEE <LEEJAE002@bellinghamschools.org>
2018-09-25 14:32:09 -0700

    ITS WORKS YEAS BRODERSgit add deadencodersyee.py git add deadencodersyee.py !

--- deadencodersyee.py
+++ deadencodersyee.py
@@ -7,20 +7,22 @@ class MyRobot (wpilib.IterativeRobot):
     def robotInit(self):
         self.talon = ctre.WPI_TalonSRX(0)
         self.talon.configSelectedFeedbackSensor(ctre.FeedbackDevice.QuadEncoder,0,0)
-
         self.oldnumber = 0
         self.tick = 0
 
     def teleopPeriodic(self):
         newnumber = self.talon.getSelectedSensorPosition(0)
         self.talon.set(1)
-        if abs(newnumber) + 1 == self.oldnumber or abs(newnumber) - 1 == self.oldnumber or abs(newnumber) == self.oldnumber:
+        if newnumber + 1 == self.oldnumber or newnumber - 1 == self.oldnumber or newnumber == self.oldnumber:
             self.tick += 1
+        else:
+            self.tick = 0
             self.oldnumber = newnumber
-        if not abs(newnumber) + 1 == self.oldnumber or not abs(newnumber) - 1 == self.oldnumber or not abs(newnumber) == self.oldnumber:
-            self.tick == 0
-        if self.tick == 50:
+        if self.tick >= 50:
             print ("It dead broder")
-
+            self.talon.set(0)
+        print (newnumber)
+        print (self.oldnumber)
+        print (self.tick)
 if __name__ == "__main__":
     wpilib.run(MyRobot, physics_enabled=True)
