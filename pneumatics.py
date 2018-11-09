88ccbdc397b5201232bd24e8969e4192aecd324b
Jacob van't Hoog <vantjac@gmail.com>
2018-11-08 17:39:31 -0800

    Clean up pneumatics code

--- pneumatics.py
+++ pneumatics.py
@@ -1,17 +1,11 @@
-import math
 import wpilib
-import ctre
-import seamonsters as sea
-import swervebot_app
 
 class Pneumatics(wpilib.IterativeRobot):
-    
-    
-    
+
     def robotInit(self):
         self.pneu = wpilib.Solenoid(0)
         self.Joystick = wpilib.Joystick(0)
-    
+
     def teleopPeriodic(self):
         if self.Joystick.getRawButton(8):
             self.pneu.set(True)
